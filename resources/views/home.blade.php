d7b76a5d7690f60d5f60eedab99f8e9695bdeb1f
ihsan <ihsan@LAPTOP-HD9J2C15.com>
2022-11-11 16:02:11 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="profil.png" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Ihsan Perdana Putra</p>
+           <p class="regular">Cyber Security | Gamers</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Aceh <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +6281368179194
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               ihsanperdanaputra@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+              Strategy and analysis
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Python
+             </div>
+             <div class="skill_progress">
+               <span style="width: 60%;"></span>
+             </div>
+             <div class="skill_per">60%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Canva
+             </div>
+             <div class="skill_progress">
+               <span style="width: 90%;"></span>
+             </div>
+             <div class="skill_per">90%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Firs Aid
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Ethical Hacking
+             </div>
+             <div class="skill_progress">
+               <span style="width: 88%;"></span>
+             </div>
+             <div class="skill_per">88%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 80%;"></span>
+                  </div>
+                  <div class="skill_per">80%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 50%;"></span>
+                </div>
+                <div class="skill_per">50%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Arabic
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 90%;"></span>
+                </div>
+                <div class="skill_per">90%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is Ihsan Perdana Putra, I'm 22 Years Old, I From Aceh singkil, Aceh, Indonesia</p>
+        <p>I Graduated from Stat Islamic University of Ar-Raniry From Major information technology</p>
+        <p>My Hoby is Playing Games, Watching movie, and Eat</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+                <div class="date">2007-2012</div> 
+                <div class="info">
+                     <p class="semi-bold">SDN 1 singkil</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2012-2015</div>
+              <div class="info">
+                     <p class="semi-bold">MTsN singkil</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2015 - 2019</div>
+              <div class="info">
+                     <p class="semi-bold">Pompes Mawaridussalam</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2020 - 2022</div>
+              <div class="info">
+                     <p class="semi-bold">Stat Islamic University of Ar-Raniry , Aceh</p> 
+                  <p>Major: information technology</p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Achievement</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2016 - 2016</div> 
+                <div class="info">
+                     <p class="semi-bold">participant jambore raimuna in pompes gontor 2 ponorogo</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2018 - 2018</div>
+              <div class="info">
+                <p class="semi-bold">leader of kostramasa in event minangkabau internasional scout camp(MISC)</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2017 - 2018</div>
+              <div class="info">
+                  <p class="semi-bold">chief coordinator</p> 
+                  
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class='fab fa-bitcoin'></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class='fas fa-biohazard'></i></li>
+         <li><i class='fas fa-hiking'></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
